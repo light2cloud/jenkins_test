43edc2169af7a84eaa2b5fdba9d8a7cdeb8dd8ce
light2cloud <32213749+light2cloud@users.noreply.github.com>
2019-01-18 10:59:06 +0800

    Update hellow.js

--- hellow.js
+++ hellow.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World 123456\n');
+    response.end('Hello World 12345678\n');
 }).listen(8888);
 
 console.log('Server running at http://127.0.0.1:8888/');

3e3203d2acb1778fb6027f9a7735a56aac285e0a
Ubuntu <ubuntu@ip-10-0-0-158.cn-northwest-1.compute.internal>
2019-12-12 07:08:55 +0000

    test

--- hellow.js
+++ hellow.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World 12345678123123\n');
-}).listen(8888);
+}).listen(8889);
 
 console.log('Server running at http://127.0.0.1:8888/');
